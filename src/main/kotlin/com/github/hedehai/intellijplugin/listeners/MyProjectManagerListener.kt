94b70ef79cf50feb0c6a496c370d4baa14a637db
GitHub Action <action@github.com>
2022-02-22 05:57:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/hedehai/intellijplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/hedehai/intellijplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.hedehai.intellijplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.hedehai.intellijplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/hedehai/intellijplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/hedehai/intellijplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.hedehai.intellijplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.hedehai.intellijplugin.MyBundle
 
 class MyApplicationService {
 
